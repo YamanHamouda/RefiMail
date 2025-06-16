ed6e7d166f9e8d5829f55693ec7717dbe62dffdf
YamanHamouda <mhdyamanhamouda21@gmail.com>
2025-06-16 10:38:06 -0700

    added code to get the email

---
+++ webapp/home.js
@@ -0,0 +1,14 @@
+const form = document.getElementById('form')
+let email;
+const get_email = (event) => 
+{
+    event.preventDefault();
+   
+    email = document.getElementById('email').value;
+
+    console.log(email);
+
+    alert("email submitted:" + email);
+}
+
+form.addEventListener('submit',get_email);
