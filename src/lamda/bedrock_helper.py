7d2b213c2ed7bc8b4b67306832033da57f0df557
YamanHamouda <mhdyamanhamouda21@gmail.com>
2025-07-01 03:25:56 -0700

    updated lamda function for more effeciency

--- src/lamda/bedrock_helper.py
+++ src/lamda/bedrock_helper.py
@@ -1,18 +1,16 @@
-#THIS FILE ISN'T ACTUALLY BEING RUN ANYWHERE IN THE REPO, BUT RATHER IT'S BEING RUN ON THE LAMDA INSTANCE
-
 import boto3
+#constants
+bedrock = boto3.client(service_name='bedrock-runtime',region_name = "us-west-2")
+modelId = "anthropic.claude-3-5-haiku-20241022-v1:0"
+prompt_template = "Make this email sound {}, don't use all caps. Only return the email. You don't use --. You don't use \"I hope this email finds you well\" \n\n{}"
+inferenceConfig={"maxTokens": 512, "temperature": 0.5, "topP": 0.9}
 
 def call_bedrock(prompt_and_email):
-    prompt = "Make this email sound {}, don't use all caps. Only return the email. You don't use --. You don't use \"I hope this email finds you well\" \n\n{}"
-    prompt = prompt.format(*prompt_and_email)
+    
+    prompt = prompt_template.format(*prompt_and_email)
 
-
-    modelId = "anthropic.claude-3-5-haiku-20241022-v1:0"
     message = generate_message(prompt)
-    inferenceConfig={"maxTokens": 512, "temperature": 0.5, "topP": 0.9}
 
-
-    bedrock = boto3.client(service_name='bedrock-runtime',region_name = "us-west-2")
     print("calling bedrock")
     response = bedrock.converse(
         modelId=modelId,
--- src/lamda/lamda_function.py
+++ src/lamda/lamda_function.py
@@ -1,28 +1,26 @@
-#THIS FILE ISN'T ACTUALLY BEING RUN ANYWHERE IN THE REPO, BUT RATHER IT'S BEING RUN ON THE LAMDA INSTANCE
-
 import json
 import boto3
 from bedrock_helper import call_bedrock
+s3 = boto3.client('s3')
+bucket = "yaman-hamouda-aws-lambda-hackathon-output"
 
 def lambda_handler(event, context):
-    
-    prompt = "Make this email {}, don't use all caps. Only return the email. You don't use --. You don't use \"I hope this email finds you well\" \n\n{}"
-    
 
 
-
-    s3 = boto3.client('s3')
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
     
     try:
         response = s3.get_object(Bucket=bucket, Key=key)
+        filename = key
         file = response['Body']
+        #11 because input_email is 11 characters long
+        id = filename[11:len(filename)-4] # len(filename) - 4 for the .txt
         prompt_type = file.readline().decode('utf-8')
         email = file.read().decode('utf-8')
         
         response = call_bedrock((prompt_type,email))
-        output_new_email(response)
+        output_new_email(response,id)
         
         print("lamda finished")
         return response
@@ -34,14 +32,17 @@ def lambda_handler(event, context):
         print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
         raise e
 
-def output_new_email(email):
-    bucket = "yaman-hamouda-aws-lambda-hackathon-output"
-    s3 = boto3.client('s3')
-    file = _string_to_file(email)
-    s3.upload_file("/tmp/output_email.txt", bucket, "output_email.txt")
+def output_new_email(email,id):
+    s3.put_object(Body=email, Bucket=bucket, Key=f"output_email{id}.txt")
+    # file = _string_to_file(email)
+    # print(id)
+    # s3.upload_file("/tmp/output_email.txt", bucket, f"output_email{id}.txt")
 
 def _string_to_file(email):
     file = open("/tmp/output_email.txt", "w")
     file.write(email)
     file.close()
     return file
+
+
+
